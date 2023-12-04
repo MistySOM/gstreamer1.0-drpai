b10061e67ecc81910732313c2c581631e9fd07bd
Matin Lotfaliei <matinlotfali@gmail.com>
2023-12-04 15:56:37 -0800

    Add yolo inside deeppose

--- gst-plugin/src/drpai-models/drpai_deeppose.cpp
+++ gst-plugin/src/drpai-models/drpai_deeppose.cpp
@@ -6,28 +6,34 @@
 #include "drpai_deeppose.h"
 
 void DRPAI_DeepPose::add_corner_text() {
-    DRPAI_Connection::add_corner_text();
+    yolo.add_corner_text();
 
-    std::string head_pose_str = "Head Pose: ";
-    switch (last_head_pose) {
-        case Center: head_pose_str += "Center"; break;
-        case Left: head_pose_str += "Left"; break;
-        case Down: head_pose_str += "Down"; break;
-        case Right: head_pose_str += "Right"; break;
-        case Up: head_pose_str += "Up"; break;
+    corner_text.clear();
+    for (auto& s: yolo.corner_text)
+        corner_text.push_back(s);
+
+    if (!yolo.last_det.empty()) {
+        std::string head_pose_str = "Head Pose: ";
+        switch (last_head_pose) {
+            case Center: head_pose_str += "Center"; break;
+            case Left: head_pose_str += "Left"; break;
+            case Down: head_pose_str += "Down"; break;
+            case Right: head_pose_str += "Right"; break;
+            case Up: head_pose_str += "Up"; break;
+        }
+        corner_text.emplace_back(head_pose_str);
+
+        if(yawn_detected)
+            corner_text.emplace_back("Yawn Detected!");
+        if(blink_detected)
+            corner_text.emplace_back("Blink Detected!");
     }
-    corner_text.emplace_back(head_pose_str);
-
-    if(yawn_detected)
-        corner_text.emplace_back("Yawn Detected!");
-    if(blink_detected)
-        corner_text.emplace_back("Blink Detected!");
 }
 
 void DRPAI_DeepPose::extract_detections() {
     uint8_t det_size = NUM_OUTPUT_KEYPOINT;
     detection det[det_size];
-    det[0].bbox = filter_region;
+    det[0].bbox = crop_region;
     auto ret = post_process.post_process_output(drpai_output_buf.data(), det, &det_size);
 
     yawn_detected = ret & (1 << 4);
@@ -56,4 +62,26 @@ void DRPAI_DeepPose::extract_detections() {
 void DRPAI_DeepPose::open_resource(uint32_t data_in_address) {
     std::cout << "Model : Deep Pose      | " << prefix << std::endl;
     DRPAI_Connection::open_resource(data_in_address);
+    yolo.open_resource(data_in_address);
+}
+
+void DRPAI_DeepPose::release_resource() {
+    yolo.release_resource();
+    DRPAI_Connection::release_resource();
+}
+
+void DRPAI_DeepPose::run_inference() {
+    yolo.run_inference();
+
+    if (!yolo.last_det.empty()) {
+        crop_region = yolo.last_det.at(0).bbox;
+        crop(crop_region);
+        DRPAI_Connection::run_inference();
+    }
+}
+
+void DRPAI_DeepPose::render_detections_on_image(Image &img) {
+    yolo.render_detections_on_image(img);
+    if (!yolo.last_det.empty())
+        DRPAI_Connection::render_detections_on_image(img);
 }
--- gst-plugin/src/drpai-models/drpai_deeppose.h
+++ gst-plugin/src/drpai-models/drpai_deeppose.h
@@ -8,21 +8,29 @@
 
 #include "drpai_connection.h"
 #include "src/dynamic-post-process/deeppose/deeppose.h"
+#include "drpai_yolo.h"
 
 class DRPAI_DeepPose: public DRPAI_Connection {
 
 public:
     explicit DRPAI_DeepPose(bool log_detects):
-            log_detects(log_detects)
+            log_detects(log_detects),
+            yolo(false)
     {}
 
     bool log_detects = false;
     bool yawn_detected = false, blink_detected = false;
     HeadPose last_head_pose = Center;
+    Box crop_region = filter_region;
+
+    DRPAI_Yolo yolo;
 
     void open_resource(uint32_t data_in_address) override;
+    void release_resource() override;
     void add_corner_text() override;
     void extract_detections() override;
+    void run_inference() override;
+    void render_detections_on_image(Image &img) override;
 };
 
 
