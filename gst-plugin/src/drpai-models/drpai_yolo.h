c2984e76b0085f0308586c18ed7977a626ae955d
Matin Lotfaliei <matinlotfali@gmail.com>
2023-12-25 19:37:30 -0800

    Stop hardcoding the DRPAI input format. Read it from model.

--- gst-plugin/src/drpai-models/drpai_yolo.h
+++ gst-plugin/src/drpai-models/drpai_yolo.h
@@ -12,7 +12,6 @@ class DRPAI_Yolo final: public DRPAI_Connection {
 
 public:
     explicit DRPAI_Yolo(const bool log_detects):
-            DRPAI_Connection(640, 480, 3),
             log_detects(log_detects),
             det_tracker(true, 2, 2.25, 1)
     {}
