5c41328d139b9cc587ffbf8bc621e2128abab118
Matin Lotfaliei <matinlotfali@gmail.com>
2025-08-19 19:32:06 -0700

    Configure remote deployment and debugging
    Configures the remote deployment and debugging process for the target device.
    
    - Renames `remote_debug.sh` to `remote_deploy.sh` and updates it to handle only deployment.
    - Creates a new `remote_debug.sh` script for starting the gdbserver on the remote device.
    - Modifies `.vscode/tasks.json` to include a new "Remote: Deploy" task and updates the "Remote Debug" task to depend on both build and deploy tasks.
    - Renames the "Remote GDB Server: Start" task to "Remote: Start GDB Server" to match the new script name and updates .vscode/launch.json accordingly.
    - Adds an input for selecting the meson build type (debug/release).
    - Adds SSHPASS environment variable for passwordless SSH login.

--- scripts/remote_debug.sh
+++ scripts/remote_debug.sh
@@ -4,17 +4,6 @@ set -e
 
 source /workspace/.env
 
-export SSHPASS=${MISTYSOM_PASS}
-
-sshpass -e sftp -Cq ${MISTYSOM_USER}@${MISTYSOM_HOST} << EOF
-put /workspace/build/gst-app/gst-launch /home/root
-put /workspace/build/gst-plugin/libgstdrpai.so /usr/lib64/gstreamer-1.0/
-put /workspace/build/gst-plugin/src/drpai-models/drpai-yolo/libgstdrpai-yolo.so /usr/lib64/
-put /workspace/build/gst-plugin/src/drpai-models/drpai-dummy/libgstdrpai-dummy.so /usr/lib64/
-put /workspace/build/gst-plugin/src/drpai-models/drpai-mobilenet/libgstdrpai-mobilenet.so /usr/lib64/
-put /workspace/gst-plugin/src/drivers/drpai-tvm/rzv_drp-ai_tvm/obj/build_runtime/V2L/libtvm_runtime.so /usr/lib64/
-EOF
-
 sshpass -e ssh -C -L 127.0.0.1:${GDB_PORT}:127.0.0.1:${GDB_PORT} ${MISTYSOM_USER}@${MISTYSOM_HOST} << EOF
 /home/root/v4l2-init.sh ${CAMERA_WIDTH}x${CAMERA_HEIGHT}
 gdbserver :${GDB_PORT} /home/root/gst-launch "${PIPELINE}"
