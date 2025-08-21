a0c7c627bc90ccaba85320b2ca315d98c35e08e4
Kiefer Aland <kiefer.aland@mistywest.com>
2025-08-21 20:40:38 +0000

    remove ssh compression flag in remote debug bash script.

--- scripts/remote_debug.sh
+++ scripts/remote_debug.sh
@@ -5,7 +5,7 @@ set -e
 source /workspace/.env
 
 SOCK="/tmp/${MISTYSOM_USER}@${MISTYSOM_HOST}.ctl"
-SSH_CMD="sshpass -e ssh -C \
+SSH_CMD="sshpass -e ssh \
   -o ControlMaster=auto \
   -o ControlPath=${SOCK} \
   -o ControlPersist=180 \
